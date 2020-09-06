cd8767b643d068bce534995b75610fbf3784246f
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-06 09:21:06 +0700

    Add new project

---
+++ OutputFromProcess/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OutputFromProcess
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ListProcesses();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+
+		private static void ListProcesses()
+		{
+			Process[] processCollection = Process.GetProcesses();
+			int i = 0;
+			foreach (Process p in processCollection)
+			{
+				i++;
+				Console.WriteLine($"{i}\t{p.ProcessName}\t{p.MainWindowTitle}");
+			}
+		}
+	}
+}
