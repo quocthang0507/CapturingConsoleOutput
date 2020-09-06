e2e67e92b8e9c98111012cb4d355dfe6beb06e2a
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-06 07:41:12 +0700

    Update project

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace ConsoleApp
 {
 	class Program
 	{
+		private const string Path = "output.txt";
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var proc = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = @"C:\Windows\system32\cmd.exe",
+					Arguments = "/c ipconfig",
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					CreateNoWindow = true
+				}
+			};
+			proc.Start();
+			if (File.Exists(Path))
+				File.Create(Path);
+			using (StreamWriter writer = File.AppendText(Path))
+			{
+				while (!proc.StandardOutput.EndOfStream)
+				{
+					string line = proc.StandardOutput.ReadLine();
+					// do something with line
+					writer.WriteLine(line);
+				}
+			}
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
 		}
 	}
 }
